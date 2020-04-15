11ce02e356a1f005a56959a3d94d7e4631cbfce2
lekegitrepo <lekedevrepo@gmail.com>
2020-04-15 14:57:41 -0500

    add route to the page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Header from './components/headerComponent/header';
 import HomePage from './components/pages/homePage';
+import Products from './components/pages/products';
 import Footer from './components/footerComponent/footer';
 import './assets/css/default.min.css';
 
 class App extends React.Component {
   render () {
     return (
-      <div className= 'App'>
-        <Header />
-        <HomePage />
-        <Footer />
-      </div>
+      <Router>
+        <div className= 'App'>
+          <Header />
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/Products' component={Products} />
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
