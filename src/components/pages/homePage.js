8456bc61f60ad2913a2172e7ca11d731f50783d7
lekegitrepo <lekedevrepo@gmail.com>
2020-04-15 14:30:39 -0500

    add dummy text to the home page content

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -4,7 +4,12 @@ class HomePage extends React.Component {
   render () {
     return (
       <div className='container-fluid'>
-        <h6> Home page contents here </h6>
+        <h1> Home page content </h1>
+
+        <p>
+          Proident sed irure aliqua est eiusmod reprehenderit ex amet eu duis do fugiat cupidatat velit dolor mollit nisi dolore esse labore anim dolor enim proident.
+          Adipisicing deserunt do duis pariatur dolor sit irure culpa commodo ullamco cillum nulla veniam sed excepteur laborum reprehenderit in id occaecat ut tempor occaecat aliqua incididunt ut nostrud eiusmod cupidatat in sunt exercitation qui proident commodo est ad ex officia ea reprehenderit ex anim nisi consequat aliquip id nostrud in in commodo officia in consectetur ut nulla culpa cillum ut esse fugiat tempor dolor sed magna tempor excepteur pariatur do sunt velit pariatur sint pariatur et et nostrud in laborum ad labore ea incididunt et dolor in laborum sint elit minim aute minim nulla cupidatat qui ex ut et consequat ut elit irure consectetur minim elit id nostrud reprehenderit voluptate commodo veniam eu nisi tempor nulla sit ut eu velit ex dolore consequat reprehenderit dolore esse in ad sunt culpa proident ad cillum do aliquip reprehenderit esse non amet enim dolore pariatur consequat minim eiusmod sunt nulla dolor qui id nostrud nostrud dolore veniam deserunt.
+        </p>
       </div>
     )
   }
