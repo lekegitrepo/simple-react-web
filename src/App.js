a07da24abb03e67c5b7c2a9f4fcf439d747a357e
lekegitrepo <lekedevrepo@gmail.com>
2020-04-15 10:50:22 -0500

    create the header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Header from './components/headerComponent/header'
 import './assets/css/default.min.css'
 
-function App() {
+class App extends React.Component {
+  render () {
+    return (
+      <div className= 'App'>
+        <Header />
+      </div>
+    )
+  }
+}
+
+/*function App() {
   return (
     <div className="App">
       
     </div>
   );
-}
+}*/
 
 export default App;
---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <header>
+        <div className='logo'> LOGO </div>
+      </header>
+    )
+  }
+}
+
+export default Header;
