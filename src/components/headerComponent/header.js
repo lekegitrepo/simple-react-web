4100fdeee8b0f5d54f618db12aab591794514867
lekegitrepo <lekedevrepo@gmail.com>
2020-04-15 12:02:30 -0500

    style the menu links

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -7,10 +7,10 @@ class Header extends React.Component {
         <div className='logo'> LOGO </div>
         <nav>
           <ul>
-            <li><a href='#'>Home</a></li>
+            <li className='first'><a href='#'>Home</a></li>
             <li><a href='#'>Career</a></li>
             <li><a href='#'>Contact</a></li>
-            <li><a href='#'>About</a></li>
+            <li className='last'><a href='#'>About</a></li>
           </ul>
         </nav>
       </header>
