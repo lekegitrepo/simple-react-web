def18cca3f670a7d0006ed14ab8aa3d6a919a88f
lekegitrepo <lekedevrepo@gmail.com>
2020-04-26 13:28:03 -0500

    create Contact component

---
+++ src/components/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render () {
+    return (
+      <div className='contact-container'>
+        <h1> Contact </h1>
+        <form>
+          
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Contact;
