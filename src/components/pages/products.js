7ac7b3ee969e51db073d9cafc632e22809565418
lekegitrepo <lekedevrepo@gmail.com>
2020-04-28 15:19:24 -0500

    add the product content

--- src/components/pages/products.js
+++ src/components/pages/products.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import Product from './product';
 
 class Products extends React.Component {
   render () {
     return (
       <div className='container-fluid'>
         <h1> Products </h1>
-
-        <p>
-          Ut sed officia magna mollit consectetur sed sint dolor labore aliqua id ut anim id cupidatat laborum incididunt enim in aute in id.
-          Aute in eu ex laborum cupidatat incididunt incididunt nulla ut ut mollit ex minim irure in in esse aliqua veniam amet dolore.
-        </p>
+        <div className='row'> <Product /> </div>
       </div>
     )
   }
