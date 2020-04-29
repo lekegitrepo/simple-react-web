281c7d91da2c714dec0f9ec64870a9564af8fb98
lekegitrepo <lekedevrepo@gmail.com>
2020-04-29 15:13:59 -0500

    refactor: encapsulate product card content in an object

--- src/components/pages/product.js
+++ src/components/pages/product.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Image from '../../assets/img/image-1.jpg';
 
-const Product = () => {
+const Product = ({image, desc, title}) => {
  return (
     <div className="card mb-3 col-md-4">
-      <img src={Image} className="card-img-top" alt="..." />
+      <img src={image} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Interior Design</h5>
-        <p className="card-text">This is a wider card with supporting 
-        text below as a natural lead-in to additional content.</p>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{desc}</p>
         <p className="card-text">
           <small className="text-muted">Last updated 3 mins ago</small>
         </p>
--- src/components/pages/products.js
+++ src/components/pages/products.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import Product from './product';
+import Image from '../../assets/img/image-1.jpg';
+
+const contents = [
+  {image: Image,
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    title: 'Interior Design'
+  },
+  {image: Image,
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    title: 'Interior Design'
+  },
+  {image: Image,
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    title: 'Interior Design'
+  }
+]
 
 class Products extends React.Component {
   render () {
@@ -7,12 +23,7 @@ class Products extends React.Component {
       <div className='container-fluid'>
         <h1 className='text-center my-4'> Designs </h1>
         <div className='row'>
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+         {contents.map((detail) => <Product {...detail}/>)}
         </div>
       </div>
     )
