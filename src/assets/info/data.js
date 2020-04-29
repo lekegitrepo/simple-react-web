fc6d81e0b0f11b3ad22de165867c3c5e7312bbdf
lekegitrepo <lekedevrepo@gmail.com>
2020-04-29 15:24:04 -0500

    create data.js file

---
+++ src/assets/info/data.js
@@ -0,0 +1,18 @@
+import Image from '../img/image-1.jpg';
+
+const contents = [
+  {image: Image,
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    title: 'Interior Design'
+  },
+  {image: Image,
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    title: 'Interior Design'
+  },
+  {image: Image,
+    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    title: 'Interior Design'
+  }
+]
+
+export default contents;
--- src/components/pages/products.js
+++ src/components/pages/products.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import Product from './product';
-import Image from '../../assets/img/image-1.jpg';
-
-const contents = [
-  {image: Image,
-    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    title: 'Interior Design'
-  },
-  {image: Image,
-    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    title: 'Interior Design'
-  },
-  {image: Image,
-    desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    title: 'Interior Design'
-  }
-]
+import contents from '../../assets/info/data';
 
 class Products extends React.Component {
   render () {
