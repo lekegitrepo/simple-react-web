7f87e9807a9ffc8b7a2a416492ed9e0fdb957767
lekegitrepo <lekedevrepo@gmail.com>
2020-04-30 15:35:07 -0500

    set the position of the navbar to fixed

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -5,18 +5,20 @@ class Header extends React.Component {
   render () {
     return (
       <header>
-        <div className='logo'> LOGO </div>
-        <nav>
-          <ul>
-            <li className='first'><Link to='/'> Home </Link></li>
-            <li>
-              <Link to='/Products'> Products </Link>
-            </li>
-            <li><Link to='/Careers'> Careers </Link></li>
-            <li><Link to='/Contact'> Contacts </Link></li>
-            <li className='last'><Link to='/About'> About </Link></li>
-          </ul>
-        </nav>
+        <div className='header'>
+          <div className='logo'> LOGO </div>
+          <nav>
+            <ul>
+              <li className='first'><Link to='/'> Home </Link></li>
+              <li>
+                <Link to='/Products'> Products </Link>
+              </li>
+              <li><Link to='/Careers'> Careers </Link></li>
+              <li><Link to='/Contact'> Contacts </Link></li>
+              <li className='last'><Link to='/About'> About </Link></li>
+            </ul>
+          </nav>
+        </div>
       </header>
     )
   }
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,15 +1,11 @@
 import React from 'react';
+import Carousel from '../carousel/carousel';
 
 class HomePage extends React.Component {
   render () {
     return (
-      <div className='container-fluid'>
-        <h1> Home page contents </h1>
-
-        <p>
-          Proident sed irure aliqua est eiusmod reprehenderit ex amet eu duis do fugiat cupidatat velit dolor mollit nisi dolore esse labore anim dolor enim proident.
-          Adipisicing deserunt do duis pariatur dolor sit irure culpa commodo ullamco cillum nulla veniam sed excepteur laborum reprehenderit in id occaecat ut tempor occaecat aliqua incididunt ut nostrud eiusmod cupidatat in sunt exercitation qui proident commodo est ad ex officia ea reprehenderit ex anim nisi consequat aliquip id nostrud in in commodo officia in consectetur ut nulla culpa cillum ut esse fugiat tempor dolor sed magna tempor excepteur pariatur do sunt velit pariatur sint pariatur et et nostrud in laborum ad labore ea incididunt et dolor in laborum sint elit minim aute minim nulla cupidatat qui ex ut et consequat ut elit irure consectetur minim elit id nostrud reprehenderit voluptate commodo veniam eu nisi tempor nulla sit ut eu velit ex dolore consequat reprehenderit dolore esse in ad sunt culpa proident ad cillum do aliquip reprehenderit esse non amet enim dolore pariatur consequat minim eiusmod sunt nulla dolor qui id nostrud nostrud dolore veniam deserunt.
-        </p>
+      <div className='content'>
+        <Carousel />
       </div>
     )
   }
