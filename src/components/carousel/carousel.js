a9b61a734830a5ca08602a484c4fdd5889acf329
lekegitrepo <lekedevrepo@gmail.com>
2020-04-30 14:41:21 -0500

    create carousel.js file

---
+++ src/components/carousel/carousel.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ImageSlide from './image-slide';
+import Image1 from '../../assets/images/image-1.jpg';
+import Image2 from '../../assets/images/image-2.jpg';
+import Image3 from '../../assets/images/image-3.jpg';
+
+ const imgUrls = [Image1, Image2, Image3];
+
+class Carousel extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = { currentImageIndex: 0};
+
+    this.nextSlide = this.nextSlide.bind(this);
+    this.previousSlide = this.previousSlide.bind(this);
+  }
+
+  componentDidMount(){
+    this.indexCount = setInterval(
+      () => this.nextSlide(),
+      5000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.indexCount);
+  }
+
+  previousSlide () {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === 0;
+    const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  nextSlide () {
+    const index =  this.incrementIndex();
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  incrementIndex(){
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === lastIndex;
+    return shouldResetIndex ? 0 : currentImageIndex + 1;
+  }
+
+  render () {
+    return (
+      <div className="carousel">
+        <ImageSlide imageUrl={ imgUrls[this.state.currentImageIndex] } />
+      </div>
+    );
+  }
+}
+
+export default Carousel;
