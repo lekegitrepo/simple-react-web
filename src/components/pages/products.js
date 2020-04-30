abebc09f13d3730eb98f70cb4e4f3cef518c9afc
lekegitrepo <lekedevrepo@gmail.com>
2020-04-30 15:43:26 -0500

    add margin top to page space below the navbar

--- src/components/pages/products.js
+++ src/components/pages/products.js
@@ -5,7 +5,7 @@ import contents from '../../assets/info/data';
 class Products extends React.Component {
   render () {
     return (
-      <div className='container-fluid'>
+      <div className='container-fluid design-container'>
         <h1 className='text-center my-4'> Designs </h1>
         <div className='row'>
          {contents.map((detail) => <Product {...detail}/>)}
